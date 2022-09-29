d493c53e16cc0154abb136a1a377d31762c0a4bf
Manuel Blanco <manuel.blanco@edgeuno.com>
2022-09-29 13:38:42 -0500

    chip colors

--- src/components/AlertList.vue
+++ src/components/AlertList.vue
@@ -131,31 +131,32 @@
             <span
               v-if="col == 'status'"
             >
-              <span
-                class="label"
+              <v-chip
+                :color="`${isDark ? 'red' : 'white'}`"
+                small
+                text-color="#000000"
                 :style="fontStyle"
               >
                 {{ props.item.status | capitalize }}
-
-              </span>
+              </v-chip>
+            </span>
+            <span
+              v-if="showNotesIcon"
+            >
               <span
-                v-if="showNotesIcon"
+                v-if="lastNote(props.item)"
+                class="pl-2"
               >
-                <span
-                  v-if="lastNote(props.item)"
-                  class="pl-2"
-                >
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-icon
-                        v-bind="attrs"
-                        small
-                        v-on="on"
-                      >text_snippet</v-icon>
-                    </template>
-                    <span>{{ lastNote(props.item) }}</span>
-                  </v-tooltip>
-                </span>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      v-bind="attrs"
+                      small
+                      v-on="on"
+                    >text_snippet</v-icon>
+                  </template>
+                  <span>{{ lastNote(props.item) }}</span>
+                </v-tooltip>
               </span>
             </span>
             <span
@@ -762,6 +763,67 @@ export default {
 }
 </script>
 
+<style >
+
+  tr {
+    border: none !important
+  }
+
+  tr.security{
+    background-color:#0288FF !important;
+  }
+  
+  tr.critical{
+    background-color:#FF6557 !important;
+  }
+  
+  tr.major{
+    /* background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23404' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23505'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E"); */
+    background-color:#FFB350 !important;
+  }
+  
+  tr.minor{
+    background-color: #F4FA1F!important;
+  }
+
+  tr.warning{
+    background-color:#5BBEFF !important;
+  }
+
+  tr.indeterminate{
+    background-color:#BCFCFF !important;
+  }
+
+  tr.cleared{
+    background-color:#35CC41 !important;
+  }
+
+  tr.normal{
+    background-color:#35CC41 !important;
+  }
+
+  tr.ok{
+    background-color:#35CC41 !important;
+  }
+
+  tr.informational{
+    background-color:#35CC41 !important;
+  }
+
+  tr.debug{
+    background-color:#FF62CF !important;
+  }
+
+  tr.trace{
+    background-color:#8A5EFF !important;
+  }
+
+  tr.unknown{
+    background-color:#C0C0C0 !important;
+  }
+</style>
+
+
 <style>
 .alert-table .v-table th, td {
   padding: 0px 5px !important;
@@ -786,7 +848,8 @@ export default {
 }
 
 .alert-table .v-table tbody td {
-  border-top: 1px solid rgb(221, 221, 221);
+  cursor: pointer;
+  border-bottom: 1px dashed rgb(250 220 168) !important;
 }
 
 .fixed-table {
